bc65791cffa1c01d592abab9a12715e7ed92ba73
psaulnie <saulnier254@gmail.com>
2023-03-30 16:08:09 +0200

    task03

---
+++ task03/corrected_main.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+int do_work(int *my_int, int retval)
+{
+	int x;
+	int y = *my_int;
+	int z;
+	for (x = 0; x < my_int; ++x) {
+		udelay(10);
+	}
+	if (y < 10)
+		pr_info("We slept a long time!");
+	z = x * y;
+	return z;
+}
+
+static int  __init my_init(void)
+{
+	int x = do_work(&x, x);
+	return x;
+}
+
+static void __exit my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
